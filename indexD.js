4c0daa7645236c47f2973272cea67162cf9e433d
Bryan Stauning <bryanstauning@gmail.com>
2023-07-26 19:55:26 -0500

    Class SVG and Shape are now working

--- indexD.js
+++ indexD.js
@@ -1,14 +1,20 @@
 const inquirer = require("inquirer")
 const fs = require("fs");
-const { Circle, Triangle, Square } = require("./lib/shape")
-const SVG = require("./lib/svg")
-console.log("Welcome to the Logo Generator!");
-console.log("Please answer the following questions to generate your logo.");
-console.log("You can choose up to 3 characters for your logo text.");
-console.log("You can choose from 3 shapes: circle, triangle, or square.");
-console.log("You can choose from 3 colors for your text: white, black, or yellow.");
-console.log("You can choose from 3 colors for your shape: green, red, or blue.");
-console.log("--------------------------------------------------------------");
+// impor the three classes from /lib/shape
+const { Circle, Triangle, Square } = require("./lib/shape");
+
+//const shapeLib = require("./lib/shape")
+const Svg = require("./lib/svg")            
+
+
+// const SVG = require("./lib/svg")
+// console.log("Welcome to the Logo Generator!");
+// console.log("Please answer the following questions to generate your logo.");
+// console.log("You can choose up to 3 characters for your logo text.");
+// console.log("You can choose from 3 shapes: circle, triangle, or square.");
+// console.log("You can choose from 3 colors for your text: white, black, or yellow.");
+// console.log("You can choose from 3 colors for your shape: green, red, or blue.");
+// console.log("--------------------------------------------------------------");
 
 // Create an array of questions for user input
 const questions = ["Enter up to 3 characters: ", "Choose a text color: ", "Choose a shape: ", "Choose a color for the shape: "];
@@ -26,7 +32,7 @@ function writeToFile(fileName, data) {
 
 // Init prompts user for inputs using the inquirer package and the questions array
 function init() {
-    
+
     inquirer.prompt([
         {
             type: "input",
@@ -54,52 +60,50 @@ function init() {
             name: "shapeColor",
             choices: ["green", "red", "blue"]
         },
-        
+
     ])
-    //
-    .then(function (data) {
-        console.log("Thank you for using the Logo Generator!");
-        console.log("data", data);
-        console.log("data.logoText", data.logoText);
-        console.log("data.textColor", data.textColor);
-        console.log("data.shapeChoice", data.shapeChoice);
-        console.log("data.shapeColor", data.shapeColor);
+        .then(function (data) {
+            // create new instance of shape class
+            //const shape = new circle();
+            console.log(data);
+            const {logoText, textColor, shapeChoice,shapeColor} = data;
 
+            // define the shape string const up here
+            let str;
 
+            // define a variable to hold the final svg string with all the values filled
+            let finalSvgString;
 
-
-        if (data.shapeChoice === "circle") {
+            // lets first get the string which completely defines our shape, i.e.
+            // whether it's a circle, a triangle, or a square
+            switch (shapeChoice) {
+                case "circle":
+                    const myCircle = new Circle();
+                    // passin the color choice the user selected for the shape
+                    myCircle.setColor(shapeColor); // I've now set the color of my shape
+                    // nowI want to return the shape string and store it in a const
+                    str = myCircle.render(); // this will return the circle element string
+                    // now I want to create my svg object
+                    const mySvg = new Svg(textColor, logoText); // at this moment, I've created my svg object with the text color and text content defined
+                    // now call the render method of my svg class
+                    finalSvgString = mySvg.render(str);
+                    // now let's write this string to a file
+                    writeToFile('logo.svg', finalSvgString);
+                    break;
             
-            var shapeString = `<circle  cx="100" cy="100" r="75" fill="${data.shapeColor}" />`
-        }
-        else if (data.shapeChoice === "square") {
-            var shapeString =  `<rect x="0" y="0" width="200" height="175"  fill="${data.shapeColor}"/>`
-        }
-        else if (data.shapeChoice === "triangle") {
-            console.log("triangle");
-            var shapeString = `<polygon points="100, 1 200, 152 0, 150" fill="${data.shapeColor}"/>`
-        }
+                default:
+                    break;
+            }
 
-        console.log(shapeString, "shapeStringxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            // let svg = new Svg(shapeColor,textColor,textString,shapeString);
+            // console.log(svg);
 
+            // console.log("data.textColor", data.textColor);
+           
+            
 
-    const svgString = 
-        `<svg width="300" height="200" >
-        ${shapeString}
-        <text text-anchor="middle" alignment-baseline="middle" font-size="3em" x="100" y="100" fill="${data.textColor}">${data.logoText}</text>
-        </svg>`
-
-    writeToFile("logo.svg", (svgString));
-
-console.log(svgString, "svgString");
-
-
-
-
-    });
-
-    
-    }// end of init function
-
+            })
+     
+    }
 
 init();      
